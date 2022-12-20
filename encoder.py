e0c612711be1616eab459489f07d5341a701ecb7
Minxrod <minxrod@gmail.com>
2022-12-19 23:32:26 -0500

    Encoding now reads input as utf8 and not binary; most characters mapped
    Basic decoder for PRG

--- encoder.py
+++ encoder.py
@@ -1,30 +1,81 @@
-from ptc_file import PTCFile, PRG_TYPE, to_bytes, md5
+from ptc_file import PTCFile, to_bytes, md5
+from ptc_file import PRG_TYPE, MEM_TYPE
 import qrcode
 import zlib
 from math import ceil
 
+CR=ord('\r')
+
+CHARS =  "\0🅐🅑����☺☻⇥★🖛🅻\r��♪♫🆁��🭽🭶🭾🅧🅨⭗�⭢⭠⭡⭣"
+CHARS += "".join([chr(c) for c in range(32,128)])
+CHARS += "◇▘▝▀▖▌▞▛▗▚▐▜▄▙▟█┻┳┣╋┫━┃█┏┓┗┛◢◣◥◤"
+CHARS += "～。「」、・ヲァィゥェォャュョッーアイウエオカキクケコサシスセソ"
+CHARS += "タチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワン゛゜"
+CHARS += "■●▲▼□○△▽��������♠♥♦♣🯅���▔▏▕▁╱╲╳▒"
+
+
 def create_internal_name(name):
 	return name.replace("\\","/").split("/")[-1].split(".")[0].upper()[:8].encode()
 
-def encode_text(filename, internal_name=None):
-	with open(filename,"rb") as f:
-		data = f.read()
+def encode_text(filename, line_end=None, internal_name=None):
+	with open(filename, "r", encoding="utf-8") as f:
+		data = list(f.read())
 	
 	if not internal_name:
 		internal_name = create_internal_name(filename)
 	
-	return PTCFile(data=data, type=PRG_TYPE, name=internal_name)
+	# line ending conversion
+	if not line_end:
+		i = 0
+		while i < len(data):
+			# find line ending if not set
+			if data[i] == '\n':
+				data[i] = '\r'
+			i += 1
+		# line_end may be None if this is a MEM type being converted with no newlines, for example
+	
+	try:
+		byte_data = bytes([CHARS.index(c) for c in data])
+	except ValueError as e:
+		print("Error: File contains characters not known in PTC character set")
+		raise e
+	
+	return PTCFile(data=byte_data, type=PRG_TYPE, name=internal_name)
 
-def encode(filename, output, force_type=None):
+def encode(filename, output, line_ending, force_type=None):
 	extension = filename.split(".")[-1]
 	if extension in ["txt"]:
-		return encode_text(filename, output)
+		return encode_text(filename, line_ending, output)
 	elif extension in ["png", "bmp"]:
 		return encode_image(filename, output)
 	elif force_type:
 		return encode_text(filename, output)
 	else:
 		raise Exception("Format type not specified and cannot be guessed")
+
+def decode_text(data):
+	unicode_str = ""
+	for c in data:
+		if c > 0 and c < 256:
+			unicode_str += CHARS[c]
+		elif c >= 256:
+			unicode_str += c
+	return unicode_str
+
+def decode(filename, output):
+	"""
+	Newlines are converted to LF.
+	"""
+	ptc = PTCFile(file=filename)
+	if ptc.type_str == PRG_TYPE or ptc.type_str == MEM_TYPE:
+		with open(output, "w") as f:
+			if ptc.type_str == PRG_TYPE:
+				s = decode_text(ptc.data)
+				f.write(s)
+			elif ptc.type_str == MEM_TYPE:
+				s = decode_text(ptc.data.decode("usc2"))
+				f.write(s)
+		
 	
 # shoutouts to this document here for all of the information
 # https://gist.github.com/ajc2/25258be3296847bc55cec9e27d13f053
--- ptctools.py
+++ ptctools.py
@@ -6,10 +6,11 @@ import encoder
 parser = argparse.ArgumentParser(prog="ptctools", description="PTC file conversion tools.")
 parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1 (c) 2022")
 
-parser.add_argument("action", choices=["decode","encode","qr"], help="Encode to PTC, decode from PTC, or create QR code")
+parser.add_argument("action", choices=["decode","encode","qr"], help="Encode to PTC, decode from PTC, or create QR code from PTC")
 parser.add_argument("source_file", help="Source file for action.")
 
 parser.add_argument("-f", "--format", dest="dest_format", help="Set PTC file output format.")
+parser.add_argument("-l", "--line-ending", dest="line_ending", choices=["CR","LF","CRLF"], help="Sets the line endings of non-PTC files.")
 parser.add_argument("-n", "--name", dest="internal_name", help="Sets the internal PTC filename.")
 parser.add_argument("-o", "--output", dest="output_name", help="Sets the output filename.")
 parser.add_argument("-p", "--palette", dest="palette_file", help="Set the palette file to use when encoding from an image.")
@@ -27,10 +28,21 @@ if args.action == "encode":
 	else:
 		output = encoder.create_internal_name(args.source_file).decode() + ".PTC"
 	
-	result = encoder.encode(args.source_file, args.internal_name, force_type=args.dest_format)
+	result = encoder.encode(args.source_file, args.internal_name, args.line_ending, force_type=args.dest_format)
 	result.write_file(output)
 	
 	print(result)
+elif args.action == "decode":
+	# PTC -> file
+	if args.output_name:
+		output = args.output_name
+	elif args.internal_name:
+		output = args.internal_name
+	else:
+		output = encoder.create_internal_name(args.source_file).decode()
+	
+	encoder.decode(args.source_file, output)
+	
 elif args.action == "qr":
 	# file -> QRs
 	encoder.create_qr(args.source_file)
